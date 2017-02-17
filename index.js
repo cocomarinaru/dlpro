9e4d75bdc0de89c3543da94934ea6116d96bbfb7
Ovidiu Sabou <ovidiu.sabou@elan-solutions.eu>
2017-02-17 16:03:20 +0200

    dlp first routes

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+var PORT = 80
+
+app.get("/", function (request, response) {
+    response.send("In curand : fara intermediar, direct de la proprietar !");
+});
+
+app.get("/:name", function (request, response) {
+    var name = request.params.name;
+    response.send("Hello " + name);
+});
+
+app.listen(process.env.PORT || PORT, function () {
+    console.log("Listening on port: " + PORT);
+})
