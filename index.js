f3faf53e4bd9275562825ef4151cb1889d55db4f
Ovidiu Sabou <ovidiu.sabou@elan-solutions.eu>
2017-02-28 16:10:17 +0200

    adding handlebars

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+"use strict";
+
 var express = require("express");
+var hbs = require("expres-hbs");
+
 var app = express();
-var PORT = 80
+var PORT = 80;
+
+
+app = express();
+
+// set the view engine
+app.set('view engine', 'hbs');
+
+// configure the view engine
+app.engine('hbs', hbs.express4({
+  defaultLayout: __dirname + '/views/layouts/default.hbs',
+  partialsDir: __dirname + '/views/partials',
+  layoutsDir: __dirname + '/views/layouts'
+}));
+
+// configure views path
+app.set('views', path.join(__dirname,'/views'));
+
 
 app.get("/", function (request, response) {
     response.send("In curand : fara intermediar, direct de la proprietar !");
@@ -13,4 +34,6 @@ app.get("/:name", function (request, response) {
 
 app.listen(process.env.PORT || PORT, function () {
     console.log("Listening on port: " + PORT);
-})
+});
+
+app.post("");
